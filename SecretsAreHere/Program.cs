f417f8280c355e07a2c8c945cdc0ace0fd639be2
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-09-18 10:06:57 -0700

    secret create, forget, retrieve, delete, purge

--- SecretsAreHere/Program.cs
+++ SecretsAreHere/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace SecretsAreHere
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace SecretsAreHere
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string secretName = "SuperSecret";
+            var keyVaultName = "aj0918";
+            var kvUri = $"https://{keyVaultName}.vault.azure.net";
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+            var secretValue = "banana";
+
+            Console.Write($"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...");
+            await client.SetSecretAsync(secretName, secretValue);
+            Console.WriteLine(" done.");
+
+            Console.WriteLine("Forgetting your secret.");
+            secretValue = string.Empty;
+            Console.WriteLine($"Your secret is '{secretValue}'.");
+
+            Console.WriteLine($"Retrieving your secret from {keyVaultName}.");
+            var secret = await client.GetSecretAsync(secretName);
+            Console.WriteLine($"Your secret is '{secret.Value.Value}'.");
+
+            Console.Write($"Deleting your secret from {keyVaultName} ...");
+            DeleteSecretOperation operation = await client.StartDeleteSecretAsync(secretName);
+            // You only need to wait for completion if you want to purge or recover the secret.
+            await operation.WaitForCompletionAsync();
+            Console.WriteLine(" done.");
+
+            Console.Write($"Purging your secret from {keyVaultName} ...");
+            await client.PurgeDeletedSecretAsync(secretName);
+            Console.WriteLine(" done.");
         }
+
+        //public void CreateClint(string )
     }
 }
